4b0f0c65c10b20f25358ec705650f0f9f015de28
gottfriedcharlie <93043695+gottfriedcharlie@users.noreply.github.com>
2025-10-24 16:04:32 -0400

    added folders

---
+++ groupProject/App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  groupProject
+//
+//  Created by Charlie Gottfried on 10/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
