e698a28eac5191f76f7589139a507abfc334b308
gottfriedcharlie <93043695+gottfriedcharlie@users.noreply.github.com>
2025-10-24 15:19:31 -0400

    Initial Commit

---
+++ groupProject/groupProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  groupProjectApp.swift
+//  groupProject
+//
+//  Created by Charlie Gottfried on 10/24/25.
+//
+
+import SwiftUI
+
+@main
+struct groupProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
